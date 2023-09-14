d4838f2ab72d3013517fa30859226c54f7d9fbf0
Jollineedriana <142318556+Jollineedriana@users.noreply.github.com>
2023-09-14 14:58:22 +0300

    Collectors

---
+++ assignment2 .py
@@ -0,0 +1,31 @@
+#change 80 to 89
+student_marks = [60, 80, 90, 98]
+student_marks [1] = 89
+print(student_marks)
+
+
+
+#add a new item [55] to the list
+student_marks = [60, 80, 90, 98]
+student_marks.append(55)
+print(student_marks)
+
+
+
+#find the size of the list having appended item 55
+print(len(student_marks))
+
+
+#write a python program to sum all items in the list
+total_marks = sum(student_marks)
+print(total_marks)
+
+#print the sum
+print("the sum of all the items in the list is:",total_marks )
+    
+    
+#write a python function that takes two lists and returns two if they have one common member
+list1 = input("Enter items:")
+list2 = input("Enter items:")
+   
+    
